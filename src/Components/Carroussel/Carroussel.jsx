9ce11e7563ddb5076e455702311d4bfdd16200b2
Thomas Ramillon <zerglavisse@gmail.com>
2023-07-19 13:44:10 +0200

    fin du carroussel

--- src/Components/Carroussel/Carroussel.jsx
+++ src/Components/Carroussel/Carroussel.jsx
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./_Carroussel.scss";
 import ArrowLeft from "../../Assets/arrow_left.png";
 import ArrowRight from "../../Assets/arrow_right.png";
 
 export default function Carroussel({ props }) {
-    console.log(props)
+  // State
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  function nextSlide() {
+    setCurrentSlide((currentSlide) => (currentSlide + 1) % props.pics.length);
+    console.log(props.pics.length);
+  }
+
+  function prevSlide() {
+    setCurrentSlide(
+      (currentSlide) =>
+        (currentSlide - 1 + props.pics.length) % props.pics.length
+    );
+  }
+
   return (
     <div className="container-carroussel">
-      <img className="arrow arrow_left" src={ArrowLeft} alt="arrow_left" />
-      <img className="arrow arrow_right" src={ArrowRight} alt="arrow_right" />
-      <img className="container-carroussel__img" src={props.pics[0]} alt={props.title} />
-      <p className="container-carroussel__slides-counter"></p>
-
-
+      <img
+        className="arrow arrow_left"
+        src={ArrowLeft}
+        alt="arrow_left"
+        onClick={prevSlide}
+      />
+      <img
+        className="arrow arrow_right"
+        src={ArrowRight}
+        alt="arrow_right"
+        onClick={nextSlide}
+      />
+      <img
+        className="container-carroussel__img"
+        src={props.pics[currentSlide]}
+        alt={props.title}
+      />
+      <p className="container-carroussel__slides-counter">
+        {currentSlide + 1}/{props.pics.length}
+      </p>
     </div>
   );
 }
